96483d9a3c0d3beead83bcc0b9816391c6e820c7
Edmon <112133265+githubusered@users.noreply.github.com>
2022-12-14 13:59:09 +0400

    Add files via upload

---
+++ leetcode125.js
@@ -0,0 +1,28 @@
+// Valid Palindrome
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// time complexity O(n)
+var isPalindrome = function(s) {
+    let arr = s.toLowerCase().split('')
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i].toUpperCase() == arr[i].toLowerCase()){
+            if(isFinite(arr[i]) && arr[i] !== ' '){
+                newArr.push(arr[i])
+            }
+        }else{
+            newArr.push(arr[i])
+        }
+    }
+    let newStr = newArr.join('')
+    if(newStr == newArr.reverse().join('')){
+        return true
+    }else{
+        return false
+    }
+};
+// isPalindrome('A man, a plan, a canal: Panama')
+
